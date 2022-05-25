0f2a84846db69b414cd22bce60963fbc968bbf3d
beef-e <102854627+beef-e@users.noreply.github.com>
2022-05-25 11:11:12 +0200

    File Creation and declaration pieces
    Declaration of all the pieces variables and tiles coords.

---
+++ Cscacchi.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//KNIGHTS
+char wKnight1[2]; 	//THE 1 MEANS IT STARTS ON THE WHITE TILE
+char wKnight2[2];
+char bKnight1[2];
+char bKnight2[2];
+
+//ROOKS
+char wRook1[2];		//THE 1 MEANS IT STARTS ON THE WHITE TILE
+char wRook2[2];
+char bRook1[2];
+char bRook2[2];
+
+//BISHOPS
+char wBish1[2];		//THE 1 MEANS IT STARTS ON THE WHITE TILE
+char wBish2[2];
+char bBish1[2];
+char bBish2[2];
+
+//QUEENS
+char wQueen[2];
+char bQueen[2];
+
+//KINGS
+char wKing[2];
+char bKing[2];
+
+//PAWNS GO FROM A TO H, FROM LEFT TO RIGHT IF PLAYING AS WHITE
+typedef char Pedone[2];
+Pedone wPawn [8];
+Pedone bPawn [8];
+
+typedef int tile [2];
+tile one [8];
+tile two [8];
+tile three [8];
+tile four [8];
+tile five [8];
+tile six [8];
+tile seven [8];
+tile height [8];
+
+
+int main(){
+	
+}
